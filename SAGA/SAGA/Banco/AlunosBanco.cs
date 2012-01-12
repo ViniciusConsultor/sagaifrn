48d3703d424d6d0e0e30419533be2d32a60f1831
savyovm <savyovm@fe1b52a3-15db-b1dc-eb07-f792bca53c81>
2012-01-12 23:58:24 +0000

    criação da classe de conxao relacionada com os alunos

---
+++ SAGA/SAGA/Banco/AlunosBanco.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAGA.Entidades;
+
+namespace SAGA.Banco
+{
+    class AlunosBanco
+    {
+        private SagaDataContext sagaCtx = new SagaDataContext();
+        private Alunos aluno = new Alunos();
+
+        public IEnumerable<Alunos> GetAlunos(int idTurma)
+        {
+            try
+            {
+                var alunos = from _aluno in sagaCtx.Aluno
+                             where idTurma == Convert.ToInt32(aluno.IdTurma)
+                             select _aluno;
+                return alunos;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
